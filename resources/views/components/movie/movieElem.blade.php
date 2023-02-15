2c2a295c81c6f9f26af33d04d2df52bd5180aff1
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-15 19:56:02 +0100

    Created a movieElem Components+Delete Movie Route

---
+++ resources/views/components/movie/movieElem.blade.php
@@ -0,0 +1,8 @@
+<li class="m-3">
+    <div>
+        <h5 class="d-inline">Name: </h5>{{ $movie -> name }}<br>
+        <h5 class="d-inline">Year: </h5>{{ $movie -> year }}<br>
+        <h5 class="d-inline">CashOut: </h5> {{ $movie -> cashOut}} €<br>
+    </div>
+    <a href="{{ route('movie.delete', $movie) }}">Delete</a>
+</li>

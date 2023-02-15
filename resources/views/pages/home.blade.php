5f248191a6a8572fc8cd187378a05c90e5a62d90
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-15 17:29:18 +0100

    Added <a> movie.create Route

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
 <div class="container">
+    <a href="{{ route('movie.create') }}">Create a New Movie</a>
     <h1>Movies</h1>
     @foreach ($genres as $genre)
         <h2 class="d-inline">Genre: {{ $genre -> name }}</h2>
         <ul>
-            {{-- 15) Facciamo un foreach per stampare i dati --}}
             @foreach ($genre -> movies as $movie)
                 <li class="m-3">
                     <div>

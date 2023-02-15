be25f0ee43e117bb61efad8afad50d9c581c3866
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-15 16:32:18 +0100

    Created and Recalled MovieSeeder in DataSeeder

---
+++ database/seeders/MovieSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// Importo i model "Genre, Movie, Tag"
+use App\Models\Genre;
+use App\Models\Movie;
+use App\Models\Tag;
+
+class MovieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+
+    // Dal model "Movie" richiamiamo la "factory" e tramite il foreach richiamiamo ogni "singolo Movie ($p)"
+    public function run()
+    {
+        Movie::factory()->count(100)->make()->each(function ($p) {
+
+            // Richiamiamo il model "Genre" in ordine casuale e andiamo a prendere il primo elemento casuale. FK
+            $genre = Genre::inRandomOrder()->first();
+
+            // Richiamiamo "$p" alla funzione inversa nel Model "Movie" e associamo una singola "Genre ($genre)"
+            $p->genre()->associate($genre);
+
+            // Salviamo il nuovo elemento
+            $p->save();
+
+            // Andiamo a creare un prodotto casuale e andiamo ad associarla nella Tabella Ponte "movie_tag". N a M
+            $tags = Tag::inRandomOrder()->limit(rand(1, 5))->get();
+            $p->tags()->attach($tags);
+        });
+    }
+}

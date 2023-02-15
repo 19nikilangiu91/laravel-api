a750099f179e243df0e12fc04df1461cfabe633c
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-15 19:56:53 +0100

    Migrated <li> in moveElem

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,13 +9,7 @@
         <h2 class="d-inline">Genre: {{ $genre -> name }}</h2>
         <ul>
             @foreach ($genre -> movies as $movie)
-                <li class="m-3">
-                    <div>
-                        <h5 class="d-inline">Name: </h5>{{ $movie -> name }}<br>
-                        <h5 class="d-inline">Year: </h5>{{ $movie -> year }}<br>
-                        <h5 class="d-inline">CashOut: </h5> {{ $movie -> cashOut}} €<br>
-                    </div>
-                </li>
+                @include('components.movie.movieElem')
             @endforeach
         </ul>
         <hr>
--- resources/views/pages/movie/home.blade.php
+++ resources/views/pages/movie/home.blade.php
@@ -4,13 +4,7 @@
      <div class="container">
         <h1>All Movies</h1>
             @foreach ($movies as $movie)
-                <li class="m-3">
-                    <div>
-                        <h5 class="d-inline">Name: </h5>{{ $movie -> name }}<br>
-                        <h5 class="d-inline">Year: </h5>{{ $movie -> year }}<br>
-                        <h5 class="d-inline">CashOut: </h5> {{ $movie -> cashOut}} €<br>
-                    </div>
-                </li>
+                @include('components.movie.movieElem')
             @endforeach
      </div>
 @endsection

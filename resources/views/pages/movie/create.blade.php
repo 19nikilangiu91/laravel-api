c398fedbfa5cff88c2e837c790cc8bb6c1f503f5
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-15 19:28:51 +0100

    Created foreach $genres & $tags

--- resources/views/pages/movie/create.blade.php
+++ resources/views/pages/movie/create.blade.php
@@ -7,13 +7,30 @@
     <form action="{{route('movie.store')}}" method="POST">
         @csrf
         <label for="name">Name</label>
+        {{-- Inserisco i valori in type come ho dichiarato nel validate --}}
         <input type="text" name="name">
         <br>
         <label for="year">Year</label>
-        <input type="date" name="year">
+        {{-- Inserisco i valori in type come ho dichiarato nel validate --}}
+        <input type="number" name="year">
         <br>
         <label for="cashOut">Cash-Out</label>
+        {{-- Inserisco i valori in type come ho dichiarato nel validate --}}
         <input type="number" name="cashOut">
+        <br>
+        {{-- Creo un foreach per stampare il "Genre" e nella value inserisco la chiave primaria --}}
+        <select name="genre_id">
+            @foreach ($genres as $genre)
+                <option value="{{ $genre -> id }}">{{ $genre -> name }}</option>    
+            @endforeach
+        </select>
+        <h3>Tags</h3>
+        {{-- Creo un foreach per stampare i "tag" e nella value inserisco la chiave primaria --}}
+        @foreach ($tags as $tag)
+            <input type="checkbox" name="tags[]" value={{ $tag -> id }}>
+            <label for="tags">{{ $tag -> name }}</label>
+            <br>            
+        @endforeach
         <br>
         <input type="submit" value="Create New Movie">
     </form>

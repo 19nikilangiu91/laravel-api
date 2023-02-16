f1782764d3b0b2fda725b0e6a94f1ee7de2d42a1
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-16 18:14:06 +0100

    Update data+mounted+template

--- front-end/vue-laravel-api/src/components/Movie.vue
+++ front-end/vue-laravel-api/src/components/Movie.vue
@@ -6,17 +6,21 @@ export default {
     name: "Movie",
     data() {
         return {
-
+            // Dichiaro l'array "movies" dove recupererò i dati dal json.
+            movies: []
         }
     },
     mounted() {
 
-        axios.get('http://localhost:8000/api/v1/movie/all')
+        axios.get('http://localhost:8000/api/movie/all')
             .then(res => {
 
                 const data = res.data;
                 const success = data.success;
-                const movies = data.response;
+                const movies = data.movies;
+
+                // Richiamo l'array
+                this.movies = data.movies;
 
                 console.log(movies);
             })
@@ -26,12 +30,22 @@ export default {
 </script>
 
 <template>
-    <h1>Movie</h1>
+    <h1>Movies</h1>
     <ul>
-        <li v-for="(movie, index) in movies">
-            {{ movie.name }}
+        <li v-for="movie in movies">
+            <h2>Name: </h2>{{ movie.name }} <br>
+            <h2>Year: </h2>{{ movie.year }}<br>
+            <h2>Cash Out: </h2>{{ movie.cashOut }}<br>
         </li>
 </ul>
 </template>
 
-<style scoped></style>
+<style scoped>
+h2 {
+    display: inline;
+}
+
+li {
+    margin: 10px auto;
+}
+</style>

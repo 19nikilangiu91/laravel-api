1fd7a0875871ad2e7aab99aeaaf7bd268141a3e9
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-16 18:15:19 +0100

    Imported Component

--- front-end/vue-laravel-api/src/App.vue
+++ front-end/vue-laravel-api/src/App.vue
@@ -1,6 +1,14 @@
-<script setup>
+<script>
 import Movie from './components/Movie.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Movie
+    }
+}
 </script>
+
 
 <template><Movie /></template>
 

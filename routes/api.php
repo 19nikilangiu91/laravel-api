19c6e925e009a6bd975b57d6f26ed7e6290aa95c
19nikilangiu91 <niki.langiu@gmail.com>
2023-02-20 14:05:36 +0100

    Created Test And All Movie Route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Importo il "ApiController
+use App\Http\Controllers\ApiController;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Test Api Route
+Route::get('/api/v1/test', [ApiController::class, 'test']);
+
+// All Movie Route
+Route::get('/v1/movie/all', [ApiController::class, 'getMovieWTagWGenre']);
